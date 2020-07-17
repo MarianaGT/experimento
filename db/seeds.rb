ea55bfb3f3dbb870adf53c4db4520ff74364b309
Mariana Galan <gatm870125@gmail.com>
2020-07-16 19:41:53 -0500

    Note added in seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,9 +3,13 @@ symptom2 = Symptom.create!(name: "fiebre")
 symptom3 = Symptom.create!(name: "diarrea")
 symptom4 = Symptom.create!(name: "falta de aliento")
 
+# Maneras de poblar el DB
 # disease1 = Disease.create!(name: 'Covid-19')
 # disease2 = Disease.create!(name: 'Faringitis')
 
 # symptom2.diseases << Disease.new(name: "Covid-19")
 # symptom2.diseases << disease1
 # symptom2.diseases << disease2
+
+# symptom = Symptom.find(3)
+# symptom.diseases << disease2

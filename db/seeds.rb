11e512984c66388738c1f1ad76e33d60cd3d4454
Mariana Galan <gatm870125@gmail.com>
2020-07-20 11:15:24 -0500

    Seeds in terminal: Disease is saved with the last association made in symptom.diseases << disease1. Previous associations are not being saved

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,17 +1,17 @@
 symptom1 = Symptom.create!(name: "tos seca")
 symptom2 = Symptom.create!(name: "fiebre")
-symptom3 = Symptom.create!(name: "diarrea")
-symptom4 = Symptom.create!(name: "falta de aliento")
+# symptom3 = Symptom.create!(name: "diarrea")
+# symptom4 = Symptom.create!(name: "falta de aliento")
 
 sign1 = Sign.create!(name: "dolor")
 sign2 = Sign.create!(name: "irritación")
-sign3 = Sign.create!(name: "mareo")
-sign4 = Sign.create!(name: "vómito")
+# sign3 = Sign.create!(name: "mareo")
+# sign4 = Sign.create!(name: "vómito")
 
 disease1 = Disease.create(name: 'Covid-19')
 symptom1.diseases << disease1
 symptom2.diseases << disease1
-symptom3.diseases << disease1
+# symptom3.diseases << disease1
 # disease2 = Disease.create!(name: 'Faringitis')
 
 # symptom2.diseases << Disease.new(name: "Covid-19")
